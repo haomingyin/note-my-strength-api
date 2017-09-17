17052597a868fcdbcf6b01e5b07d966383c72fad
Haoming Yin <hyi25@uclive.ac.nz>
2017-09-17 18:18:56 +1200

    Update file to test Jenkins

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ const PORT = 8088;
 http.createServer(function (req, res) {
     console.log("Listening on port: " + PORT);
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.write("<html><body><hi>Hello World!</hi></body></html>");
+    res.write("<html><body><h1>Hello World!</h1></body></html>");
     res.end();
 }).listen(PORT, function (err) {
     if (err) {
