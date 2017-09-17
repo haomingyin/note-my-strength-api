bb7207dcc9d8771a7ad84ad43dbe57a36bc89d02
Haoming Yin <hyi25@uclive.ac.nz>
2017-09-18 10:51:17 +1200

    try a different method to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+ssh -t -t haoming@haomingyin.com <<EOF
 cd /var/lib/jenkins/workspace/note-my-strength-api
-sudo npm stop
+npm stop
 export JENKINS_MODE=true
 npm install
 npm start
 exit
+EOF
