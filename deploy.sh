d6c5fadda50a41c83cf62bdd2c10b406a0acc94d
Haoming Yin <hyi25@uclive.ac.nz>
2017-09-18 00:12:26 +1200

    Refactored the way to deploy by using ssh to do the commands

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-ssh -t -t haoming@haomingyin.com << EOF
+#ssh -t -t haoming@haomingyin.com << EOF
+#  cd /var/lib/jenkins/workspace/note-my-strength-api
+#  npm stop
+#  export JENKINS_MODE=true
+#  npm install
+#  npm start
+#  exit
+#EOF
+
   cd /var/lib/jenkins/workspace/note-my-strength-api
   npm stop
   export JENKINS_MODE=true
   npm install
   npm start
   exit
-EOF
