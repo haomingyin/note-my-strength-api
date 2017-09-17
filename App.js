7d9e5d8dadf9bb113aea6b590c6fed46ff86a8e9
Haoming Yin <hyi25@uclive.ac.nz>
2017-09-17 14:56:29 +1200

    Initialized the project with very basic server listener.

---
+++ App.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const PORT = 8088;
+
+http.createServer(function (req, res) {
+    console.log("Listening on port: " + PORT);
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write("<html><body><hi>Hello World!</hi></body></html>");
+    res.end();
+}).listen(PORT, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Listening on port: " + PORT);
+    }
+});
