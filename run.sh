3a7b65fcfe75e3fba4c5330c9194eb3b42803d98
Haoming Yin <hyi25@uclive.ac.nz>
2017-09-17 22:15:40 +1200

    Added bash script file to terminate previous build and run a new instance

--- run.sh
+++ run.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
-pkill -x node-my-strength-api | cat
-
 export JENKINS_MODE=true
 
 node App.js note-my-strength-api &
 
-if [ $? -eq 0 ]
-then
-    date
-    echo "Application is running..."
-else
-    echo "Failed to run application!"
-    exit 1
-fi
 exit 0
