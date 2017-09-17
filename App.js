5c3cce308bdbd7b00642e4d5317f012b052ab6de
Haoming Yin <hyi25@uclive.ac.nz>
2017-09-17 21:39:31 +1200

    Added bash script file to terminate previous build and run a new instance

--- App.js
+++ App.js
@@ -4,13 +4,19 @@ const http = require("http");
 
 const PORT = 8088;
 
+if (process.env.JENKINS_MODE) {
+    console.log = function () {
+    }
+}
+
 http.createServer(function (req, res) {
-    console.log("Listening on port: " + PORT);
     res.writeHead(200, {"Content-Type": "text/html"});
     res.write("<html><body><h1>Hello World!</h1></body></html>");
     res.end();
 }).listen(PORT, function (err) {
     if (err) {
         console.log(err);
+    } else {
+        console.log("Listening on port: " + PORT);
     }
 });
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+pkill -x node-my-strength-api | cat
+
+node App.js note-my-strength-api &
+
+if [ $? -eq 0 ]
+then
+    date
+    echo "Application is running..."
+else
+    echo "Failed to run application!"
+    exit 1
+fi
+exit 0
