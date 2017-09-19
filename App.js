1d4cbbab6073130d08563ecc1b2e1266cda67482
Haoming Yin <hyi25@uclive.ac.nz>
2017-09-19 14:22:40 +1200

    CI testing

--- App.js
+++ App.js
@@ -6,7 +6,16 @@ const PORT = 8088;
 
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.write("<html><body><h1>Hello Yingying!</h1></body></html>");
+    res.write(`
+<html>
+<head>
+<title>Note-My-Strength API Testing Page</title>
+</head>
+<body>
+<h1>Hello Yingying!</h1>
+</body>
+</html>`
+    );
     res.end();
 }).listen(PORT, function (err) {
     if (err) {
